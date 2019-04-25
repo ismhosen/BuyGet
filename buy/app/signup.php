4602802399feccc7922b3576cd3938b82696d9e0
ismhosen <36270529+ismhosen@users.noreply.github.com>
2019-04-25 02:11:12 -0700

    fronted validation

--- buy/app/signup.php
+++ buy/app/signup.php
@@ -52,7 +52,9 @@ myHeader();
 						<div class="col-md-12">
 							<h3 class="">Sign UP Form</h3>
 							<hr>
-							
+							<div class="alert alert-danger" id="message" style="display:none">
+							 <span id="danger_text"></span>
+							</div>
 							<div class="table-responsive">
 								<table align="center" class="table-class">
 									<tbody>
@@ -74,7 +76,7 @@ myHeader();
 											</td>
 											<td>	
 												<hr>
-												<input type="text" class="myinput input-sm"  placeholder="&nbsp;Enter your Full Name..." name="fullname" size="50" value="<?php echo $_SESSION['name'];?>">
+												<input type="text" id="nameinput" class="myinput input-sm"  placeholder="&nbsp;Enter your Full Name..." name="fullname" size="50" value="<?php echo $_SESSION['name'];?>">
 												<span class="error"><?php echo $_GET['msgname']?></span>
 											</td>		
 										</tr>
@@ -85,7 +87,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="text" class="myinput input-sm" name="email" placeholder="&nbsp;Enter your Email here..." value="<?php echo $_SESSION['email'];?>">
+												<input type="text" id="emailinput" class="myinput input-sm" name="email" placeholder="&nbsp;Enter your Email here..." value="<?php echo $_SESSION['email'];?>">
 												<span class="error"><?php echo $_GET['msgemail']?></span>
 											</td>
 										</tr>
@@ -96,7 +98,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="text" class="myinput input-sm" name="password" placeholder="&nbsp;Paswword..." value="<?php echo $_SESSION['password'];?>">
+												<input type="text" id="passwordinput" class="myinput input-sm" name="password" placeholder="&nbsp;Paswword..." value="<?php echo $_SESSION['password'];?>">
 												<span class="error"><?php echo $_GET['msgpass'];?></span>
 											</td>
 										</tr>
@@ -107,7 +109,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="text" class="myinput input-sm"  placeholder="&nbsp;Enter your paswword..." name="cpassword" value="<?php echo $_SESSION['cpassword'];?>">
+												<input type="text" id="cpasswordinput" class="myinput input-sm"  placeholder="&nbsp;Enter your paswword..." name="cpassword" value="<?php echo $_SESSION['cpassword'];?>">
 												<span class="error"><?php echo $_GET['msgpassc'];?></span>
 											</td>
 										</tr>
@@ -118,7 +120,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="text" class="myinput input-sm"  placeholder="&nbsp;Enter mobile no ..." name="phone" value="<?php echo $_SESSION['phone']?>">
+												<input type="text" id="phoneinput" class="myinput input-sm"  placeholder="&nbsp;Enter mobile no ..." name="phone" value="<?php echo $_SESSION['phone']?>">
 												<span class="error"><?php echo $_GET['msgphone'];?></span>
 											</td>
 										</tr>
@@ -129,7 +131,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="date" class="myinput input-sm" name="date" value="<?php echo $_SESSION['date'];?>">
+												<input type="date" id="dobinput" class="myinput input-sm" name="date" value="<?php echo $_SESSION['date'];?>">
 												<span class="error"><?php echo $_GET['msgdate'];?></span>
 											</td>
 										</tr>
@@ -154,7 +156,7 @@ myHeader();
 											</td>
 											<td>
 												<hr>
-												<input type="text" class="myinput input-sm"  placeholder="&nbsp;Flat/House No/Road No/Area ..." name="address" value="<?php echo $_SESSION['address'];?>">
+												<input type="text" id="addressinput" class="myinput input-sm"  placeholder="&nbsp;Flat/House No/Road No/Area ..." name="address" value="<?php echo $_SESSION['address'];?>">
 												<span class="error"><?php echo $_GET['msgaddress'];?></span>
 												<hr>
 											</td>
@@ -165,7 +167,7 @@ myHeader();
 								<input type="checkbox" name="checkBox">&nbsp;&nbsp;By creating an account you agree to our <a href="signup.php">Terms & Privacy</a>.
 								<span class="error"><?php echo $_GET['msgcheckbox'];?></span>
 								<hr>
-								<button type="submit" class="btn btn-success" name="submit">Submit</button><br><br><br>
+								<button type="submit" class="btn btn-success" name="submit" onclick="return validation()">Submit</button><br><br><br>
 							</div>
 							
 						</div>
@@ -173,6 +175,8 @@ myHeader();
 				</div>
 			</form>
 		</div>
+
+		
 	</body>
 </html>
 	
@@ -181,3 +185,71 @@ session_unset();
 //session_destroy();
 myFooter();
 ?>
+
+<script>
+	function validation()
+	{
+		var name=document.getElementById("nameinput").value;
+		var email=document.getElementById("emailinput").value;
+		var password=document.getElementById("passwordinput").value;
+		var cpassword=document.getElementById("cpasswordinput").value;
+		var phone=document.getElementById("phoneinput").value;
+		var dob=document.getElementById("dobinput").value;
+		var address=document.getElementById("addressinput").value;
+		var msg_text=document.getElementById("danger_text");
+		var message=document.getElementById("message");
+		var count=0;
+		msg="Please Check ";
+		if(name=="")
+		{
+			msg+="Name";
+			count++;			
+		}
+		else if(email=="")
+		{
+			msg+="Email";
+			count++;
+		}
+		else if(password=="")
+		{
+			msg+="Password";
+			count++;
+		}
+		else if(cpassword=="")
+		{
+			msg+="Confirm Password";
+			count++;
+		}
+		else if(phone=="")
+		{
+			msg+="Phone";
+			count++;
+		}
+		else if(dob=="")
+		{
+			msg+="Date of birth";
+			count++;
+		}
+		else if(address=="")
+		{
+			msg+="Address";
+			count++;
+		}
+		else
+		{
+			message.style.display = "none";
+		}
+		if(count>0)
+		{
+			message.style.display = "block";
+			msg_text.innerHTML=msg;
+			alert(msg);
+		}
+		// if(count>0)
+		// {
+		// 	message.style.display = "block";
+		// 	message.innerHTML="block";
+		// 	// alert("hello");
+		// }
+	}
+</script>
